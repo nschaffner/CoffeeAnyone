fdb20954d69218998428ff7a44f0a2da22cb29e3
Eric Armstrong <earmstrong81@gmail.com>
2020-02-07 08:28:31 -0800

    .gitignore fix

--- MidTermCheck/CoffeeAnyone/Pods/Target Support Files/Pods-CoffeeAnyone-CoffeeAnyoneUITests/Pods-CoffeeAnyone-CoffeeAnyoneUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CoffeeAnyone_CoffeeAnyoneUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CoffeeAnyone_CoffeeAnyoneUITests
-@end
--- MidTermCheck/CoffeeAnyone/Pods/Target Support Files/Pods-CoffeeAnyoneTests/Pods-CoffeeAnyoneTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CoffeeAnyoneTests : NSObject
-@end
-@implementation PodsDummy_Pods_CoffeeAnyoneTests
-@end
