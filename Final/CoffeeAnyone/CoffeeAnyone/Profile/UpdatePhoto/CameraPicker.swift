f10ce736faa90c8713a219ca4371d8a5926fb1be
Nick Schaffner <nlschaffner@gmail.com>
2020-02-28 16:28:29 -0800

    Face Detection w/o Pods

--- Final/CoffeeAnyone/CoffeeAnyone/Profile/UpdatePhoto/CameraPicker.swift
+++ Final/CoffeeAnyone/CoffeeAnyone/Profile/UpdatePhoto/CameraPicker.swift
@@ -7,16 +7,20 @@
 //
 
 import SwiftUI
-import FirebaseStorage
+import Firebase
 
 class CameraPickerCoordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     @Binding var isShown: Bool
     @Binding var image: Image?
+    @Binding var text: Text?
+    let options = VisionFaceDetectorOptions()
+    lazy var vision = Vision.vision()
     
-    init(isShown: Binding<Bool>, image: Binding<Image?>) {
+    init(isShown: Binding<Bool>, image: Binding<Image?>, text: Binding<Text?>) {
         _isShown = isShown
         _image = image
+        _text = text
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -34,6 +38,24 @@ class CameraPickerCoordinator: NSObject, UINavigationControllerDelegate, UIImage
             print("Success")
         }
         
+        options.performanceMode = .accurate
+        options.landmarkMode = .all
+        options.classificationMode = .all
+        
+        let faceDetector = vision.faceDetector(options: options)
+        let newImage = VisionImage(image: uiImage)
+        //var newText = "Face Detected"
+        //var count = 1
+        faceDetector.process(newImage) { faces, error in
+            guard error == nil, let faces = faces, !faces.isEmpty else {
+                print("Print: No Face Detected")
+                //newText = "No Face Detected"
+                self.text = Text("No Face Detected")
+                return
+            }
+            print("Print: Face Detected")
+            self.text = Text("Face Detected")
+        }
         
         image = Image(uiImage: uiImage)
         isShown = false
@@ -50,13 +72,14 @@ struct CameraPicker: UIViewControllerRepresentable {
     
     @Binding var isShown: Bool
     @Binding var image: Image?
+    @Binding var text: Text?
     
     func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<CameraPicker>) {
         
     }
     
-    func makeCoordinator() -> ImagePickerCoordinator {
-        return ImagePickerCoordinator(isShown: $isShown, image: $image)
+    func makeCoordinator() -> CameraPickerCoordinator {
+        return CameraPickerCoordinator(isShown: $isShown, image: $image, text: $text)
     }
     
     func makeUIViewController(context: UIViewControllerRepresentableContext<CameraPicker>) -> UIImagePickerController {
